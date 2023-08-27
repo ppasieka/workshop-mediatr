ae2bade73eb2a0efe7c04b5d587c24199a79801a
Piotr Pasieka <ppasieka@gmail.com>
2023-08-27 23:10:08 +0200

    Add collection types

--- QuizShop/QuizService/Application/QuizWithQuestions.cs
+++ QuizShop/QuizService/Application/QuizWithQuestions.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 
 namespace QuizService.Application;
@@ -6,7 +7,7 @@ internal class QuizWithQuestions
 {
     public QuizId Id { get; init; }
     public QuizTitle Title { get; init; }
-    public IReadOnlyList<Question> Questions { get; init; }
+    public QuestionsCollection Questions { get; init; } = new();
 }
 
 internal class Question
@@ -14,11 +15,30 @@ internal class Question
     public QuestionId Id { get; init; }
     public QuestionText Text { get; init; }
     public AnswerId? CorrectAnswerId { get; init; }
-    public IReadOnlyList<Answer> Answers { get; init; }
+    public AnswersCollection Answers { get; init; } = new();
 }
 
+internal class QuestionsCollection : IEnumerable<Question>
+{
+    private readonly List<Question> _questions = new();
+    public IReadOnlyCollection<Question> Questions => _questions;
+    internal void Add(Question question) => _questions.Add(question);
+    internal void Add(IEnumerable<Question> questions) => _questions.AddRange(questions);
+    public IEnumerator<Question> GetEnumerator() => _questions.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
 internal class Answer
 {    
     public AnswerId Id { get; init; }
     public AnswerText Text { get ; init; }
+}
+
+internal class AnswersCollection : IEnumerable<Answer>
+{
+    private readonly List<Answer> _answers = new();
+    public IReadOnlyCollection<Answer> Answers => _answers;
+    internal void Add(Answer answer) => _answers.Add(answer);
+    internal void Add(IEnumerable<Answer> answers) => _answers.AddRange(answers);
+    public IEnumerator<Answer> GetEnumerator() => _answers.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
